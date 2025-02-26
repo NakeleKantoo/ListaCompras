b3d7aa8ca31459c4d448cbdca50ecdb38415fc6e
NakeleKantoo <zzleozz2017@gmail.com>
2025-02-26 20:29:31 -0300

    Algumas mudanças

---
+++ script.js
@@ -0,0 +1,11 @@
+function change(id) {
+    let tbody = document.getElementById('tbody');
+    let tr = tbody.children[id];
+    tr.classList.add('strikeout');
+}
+
+function addItens() {
+
+}
+
+function 
